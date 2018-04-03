0214dfe313d91cac940373812c947928b9376114
evgeniym69 <evgeniym69@gmail.com>
2018-04-03 17:53:21 +0300

    Пятый урок

---
+++ lesson5/homework part1/js/script.js
@@ -0,0 +1,14 @@
+document.body.style.backgroundImage = "url('./img/apple_true.jpg')";
+
+var menu = document.querySelector('.menu'), 
+	listItem = document.createElement('li'),
+	menuItem = document.getElementsByClassName('menu-item'),
+	adv = document.querySelector('.adv'),
+	propmt = document.getElementById('prompt');
+
+listItem.innerHTML = "<li class='menu-item'>Пятый пункт</li>";
+menu.appendChild(listItem);
+menu.insertBefore(menuItem[2], menuItem[1]);
+title.textContent = "Мы продаем только подлинную технику Apple";
+adv.remove();//Вычитал на https://learn.javascript.ru/modifying-document. Или использовать другой способ и, если да, то какой?
+propmt.textContent = "Как вы относитесь к технике Apple?";
