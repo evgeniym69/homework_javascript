b89d6ba0043d9ebd1e39bb98c7e2699de3874f9a
evgeniym69 <evgeniym69@gmail.com>
2018-04-03 19:47:48 +0300

    Исправления по первой части урока 5

--- lesson5/homework part1/js/script.js
+++ lesson5/homework part1/js/script.js
@@ -4,11 +4,12 @@ var menu = document.querySelector('.menu'),
 	listItem = document.createElement('li'),
 	menuItem = document.getElementsByClassName('menu-item'),
 	adv = document.querySelector('.adv'),
-	propmt = document.getElementById('prompt');
+	propmt = document.getElementById('prompt'),
+	question = prompt("Как вы относитесь к технике Apple?", "");
 
 listItem.innerHTML = "<li class='menu-item'>Пятый пункт</li>";
 menu.appendChild(listItem);
 menu.insertBefore(menuItem[2], menuItem[1]);
 title.textContent = "Мы продаем только подлинную технику Apple";
 adv.remove();//Вычитал на https://learn.javascript.ru/modifying-document. Или использовать другой способ и, если да, то какой?
-propmt.textContent = "Как вы относитесь к технике Apple?";
+propmt.textContent = question;
