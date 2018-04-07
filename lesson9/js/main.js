933800ccdcc752d00a9821b79c860808f952be81
Evgeniy <evgeniym69@gmail.com>
2018-04-07 15:32:34 +0300

    Исправления по уроку 9

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -1,7 +1,7 @@
 let age = document.getElementById('age');
  
 function showUser(surname, name) {
-         alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+         alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
 }
- 
-showUser.call('Brown', 'Nick', age);
+
+showUser('Brown', 'Nick');
