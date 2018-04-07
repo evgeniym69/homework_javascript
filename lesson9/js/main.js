c97971b4bbf7989e0118632ba1b72d64ae11055e
Evgeniy <evgeniym69@gmail.com>
2018-04-07 15:49:42 +0300

    Исправления по this

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -1,7 +1,8 @@
 let age = document.getElementById('age');
  
 function showUser(surname, name) {
-         alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
+			this.value = age.value;
+       alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 }
 
 showUser('Brown', 'Nick');
