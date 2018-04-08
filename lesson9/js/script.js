aa0b052d640a9845979fb3dc5ae1e86e5e5bc189
Evgeniy <evgeniym69@gmail.com>
2018-04-08 03:08:39 +0300

    Исправления по 9му уроку. Добавление модального окна на табы.

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -78,7 +78,8 @@ window.addEventListener('DOMContentLoaded', function(){
 
 		var more = document.querySelector('.more'),
 				overlay = document.querySelector('.overlay'),
-				close = document.querySelector('.popup-close');
+				close = document.querySelector('.popup-close'),
+				info = document.querySelector('.info');
 
 		more.addEventListener('click', function(){
 				this.classList.add('more-splash');
@@ -90,6 +91,12 @@ window.addEventListener('DOMContentLoaded', function(){
 				overlay.style.display = "none";
 				more.classList.remove('more-splash');
 				document.body.style.overflow = 'visible';
+		});
+
+		info.addEventListener('click', function(event){
+			if(event.target && event.target.className == 'description-btn'){
+				overlay.style.display = "block";
+			}
 		});
 
 }) ;
