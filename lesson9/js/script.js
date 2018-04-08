0fb193939351d600f2447c2f2013a1711e1e0502
Evgeniy <evgeniym69@gmail.com>
2018-04-08 21:32:38 +0300

    10 урок

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -1,10 +1,10 @@
 window.addEventListener('DOMContentLoaded', function(){
-	var tab = document.getElementsByClassName('info-header-tab'),
+	let tab = document.getElementsByClassName('info-header-tab'),
 		tabContent = document.getElementsByClassName('info-tabcontent'),
 		info = document.getElementsByClassName('info-header')[0];
 
 	function hideTabContent(a){
-		for (var i = a; i < tabContent.length; i++){
+		for (let i = a; i < tabContent.length; i++){
 			tabContent[i].classList.remove('show');
 			tabContent[i].classList.add('hide');
 		}
@@ -34,10 +34,10 @@ window.addEventListener('DOMContentLoaded', function(){
 
 //Timer
 
-	var deadline = '2018-03-08';
+	let deadline = '2018-03-08';
 
 	function getTimeRemaining(endtime){
-		var t = Date.parse(endtime) - Date.parse(new Date()),
+		let t = Date.parse(endtime) - Date.parse(new Date()),
 			seconds = Math.floor((t / 1000) % 60),
 			minutes = Math.floor((t / 1000 / 60) % 60),
 			hours = Math.floor((t / (1000 * 60 * 60)));
@@ -51,13 +51,13 @@ window.addEventListener('DOMContentLoaded', function(){
 	}
 
 	function setClock(id, endtime){
-		var timer = document.getElementById(id),
+		let timer = document.getElementById(id),
 			_hours = timer.querySelector('.hours'),
 			_minutes = timer.querySelector('.minutes'),
 			_seconds = timer.querySelector('.seconds');
 
 			function updateClock(){
-				var _t = getTimeRemaining(endtime);
+				let _t = getTimeRemaining(endtime);
 					_hours.innerHTML = _t.hours;
 					_minutes.innerHTML = _t.minutes;
 					_seconds.innerHTML = _t.seconds;
@@ -65,21 +65,21 @@ window.addEventListener('DOMContentLoaded', function(){
 					if(_t.total <= 0){
 						timer.innerHTML = "00 : 00 : 00";
 					}
-			};
+			}
 
 			updateClock();
-			var timeInterval = setInterval(updateClock, 1000);
+			let timeInterval = setInterval(updateClock, 1000);
 
-	};
+	}
 
 	setClock('timer', deadline);
 
 	//Модальное окно
 
-		var more = document.querySelector('.more'),
+		let more = document.querySelector('.more'),
 				overlay = document.querySelector('.overlay'),
 				close = document.querySelector('.popup-close'),
-				info = document.querySelector('.info');
+				infoBlock = document.querySelector('.info');
 
 		more.addEventListener('click', function(){
 				this.classList.add('more-splash');
@@ -93,10 +93,11 @@ window.addEventListener('DOMContentLoaded', function(){
 				document.body.style.overflow = 'visible';
 		});
 
-		info.addEventListener('click', function(event){
+		infoBlock.addEventListener('click', function(event){
 			if(event.target && event.target.className == 'description-btn'){
 				overlay.style.display = "block";
 			}
 		});
 
 }) ;
+
