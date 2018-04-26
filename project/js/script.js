e0b0ace7b317f15a1831ef5efbb3821cec154ac3
evgeniym69 <evgeniym69@gmail.com>
2018-04-26 20:53:52 +0300

    Final project

--- project/js/script.js
+++ project/js/script.js
@@ -11,9 +11,9 @@ let createButton = document.getElementById('popup-btn'),
 		mainCardsItem = document.querySelectorAll('.main-cards-item')[1],
 		readyBtn = document.getElementById('ready'),
 		resetBtn = document.getElementById('reset'),
-		sexCustomMale = document.getElementById('male');
-		sexCustomFemale = document.getElementById('female');
-		arrMen = ["url('./img/construct-5.png')",
+		sexCustomMale = document.getElementById('male'),
+		sexCustomFemale = document.getElementById('female'),
+		arrMen = [" url('./img/construct-5.png')",
 					"url('./img/construct-6.png')",
 					"url('./img/construct-7.png')",
 					"url('./img/construct-8.png')"],
@@ -21,8 +21,8 @@ let createButton = document.getElementById('popup-btn'),
 					"url('./img/construct-2.png')",
 					"url('./img/construct-3.png')",
 					"url('./img/construct-4.png')"],
-		slideIndexMen = 1,
-		slideIndexWomen = 1,
+		slideIndex = 1,
+		//slideIndexWomen = 1,
 		preview = document.querySelector('.preview'),
 		previewMain = document.querySelector('.person-easy');
 
@@ -39,44 +39,46 @@ createButton.addEventListener('click', function(){
 
 	sexCustomMale.checked = false;//Удаляю атрибут checked у input radio
 
-//Slider
-let prev = document.querySelector('.prev'),
-	next = document.querySelector('.next');
-
-	preview.style.backgroundImage = arrMen[0];
-	previewMain.style.backgroundImage = arrMen[0];
 
 
-sexCustomMale.onclick = plusSlidesMen(slideIndexMen);
+});
 
-function showBackgroundSlidesMen(n) {
-	      if (n > arrMen.length) { slideIndexMen = 1; };
-	      if (n < 1) {slideIndexMen = arrMen.length;};
+
+
+/*sexCustomMale.addEventListener('click', function(){
+	function showBackgroundSlidesMen(n) {
+	      if (n > arrMen.length){ 
+	      	slideIndexMen = 1; 
+	      };
+	      if (n < 1) {
+	      	slideIndexMen = arrMen.length;
+	      };
 
 	      for(let i = 0; i < arrMen.length; i++){
-	      preview.style.backgroundImage = arrMen[slideIndexMen - 1],
-	     	previewMain.style.backgroundImage = arrMen[slideIndexMen - 1];
+			
+	      	preview.style.backgroundImage = arrMen[i - 1];
+	     	previewMain.style.backgroundImage = arrMen[i - 1];
 	      }
+
 	      return arrMen[slideIndexMen - 1];
 	      }
 
-	    function plusSlidesMen(n) {
+	    function plusSlides(n) {
 	      showBackgroundSlidesMen(slideIndexMen += n);
 	    }
 
 	    prev.addEventListener('click', function() {
-	      plusSlidesMen(-1);
+	      plusSlides(-1);
 	    });
 
 	    next.addEventListener('click', function() {
-	      plusSlidesMen(1);
+	      plusSlides(1);
 	    });
+}); 
 
 
-sexCustomFemale.onclick = plusSlidesWomen(slideIndexWomen);
-	
 
-
+sexCustomFemale.addEventListener('click', function(){
 	function showBackgroundSlidesWomen(n) {
       if (n > arrWomen.length) { slideIndexWomen = 1;};
       if (n < 1) { slideIndexWomen = arrWomen.length;};
@@ -86,27 +88,94 @@ sexCustomFemale.onclick = plusSlidesWomen(slideIndexWomen);
       return arrWomen[slideIndexWomen - 1];
       }
 
-    function plusSlidesWomen(n) {
+    function plusSlides(n) {
       showBackgroundSlidesWomen(slideIndexWomen += n);
     }
 
     prev.addEventListener('click', function() {
-      plusSlidesWomen(-1);
+      plusSlides(-1);
     });
 
     next.addEventListener('click', function() {
-      plusSlidesWomen(1);
+      plusSlides(1);
     });
+}); 
+	
 
+
+	*/
+
+
+//Slider
+
+
+let prev = document.querySelector('.prev'),
+	next = document.querySelector('.next');
+
+
+
+	function showSlides(n) {
+
+    if (n > arrWomen.length || n > arrMen.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = arrWomen.length;
+        slideIndex = arrMen.length;
+    }
+
+    for (let i = 0; i < arrWomen.length; i++) {
+      preview.style.backgroundImage = arrWomen[slideIndex - 1],
+      previewMain.style.backgroundImage = arrWomen[slideIndex - 1];
+
+    }
+
+    for (let i = 0; i < arrMen.length; i++) {
+        preview.style.backgroundImage = arrMen[i - 1];
+	    previewMain.style.backgroundImage = arrMen[i - 1];
+    }
+
+    if(sexCustomMale.checked) {
+
+        preview.style.background =  arrMen[[slideIndex - 1]] + 'center no-repeat #fff';
+        preview.style.backgroundSize = 'contain';
+        previewMain.style.background = arrMen[[slideIndex - 1]] + 'center no-repeat';
+        previewMain.style.backgroundSize = 'contain';
+      
+
+    } else if(sexCustomFemale.checked) {
+
+        preview.style.background = arrWomen[[slideIndex - 1]] + 'center no-repeat #fff';
+        preview.style.backgroundSize = 'contain';
+        previewMain.style.background = arrWomen[[slideIndex - 1]] + 'center no-repeat';
+        previewMain.style.backgroundSize = 'contain';
+
+    }
+
+}
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+prev.addEventListener('click', function() {
+    plusSlides(-1);
+});
+
+next.addEventListener('click', function() {
+    plusSlides(1);
 });
 
 
 //Добавление блока с кандидатом
 readyBtn.addEventListener('click', function(){
 
-let nameCustom = document.getElementById('name');
-		ageCustom = document.getElementById('age');
-		customViews = document.getElementById('select');
+let nameCustom = document.getElementById('name'),
+		ageCustom = document.getElementById('age'),
+		customViews = document.getElementById('select'),
 		bioCustom = document.getElementById('bio'); 
 
 		if(nameCustom.value == '' && ageCustom.value == ''){
@@ -127,7 +196,9 @@ let nameCustom = document.getElementById('name');
 		bioCandidate = document.querySelectorAll('.bio')[2];
 		photoCandidate = document.querySelectorAll('.photo-2')[1];
 
-		
+
+
+
 
 
 
@@ -171,10 +242,10 @@ let nameCustom = document.getElementById('name');
 		bioCandidate.textContent = bioCustom.value;
 
 		if (sexCustomMale.checked){
-			photoCandidate.style.background = arrMen[slideIndexMen - 1] + 'no-repeat center center / 90%';
+			photoCandidate.style.background = arrMen[slideIndex - 1] + 'no-repeat center center / 90%';
 		}
 		else{
-			photoCandidate.style.background = arrWomen[slideIndexWomen - 1] + 'no-repeat center center / 76%';
+			photoCandidate.style.background = arrWomen[slideIndex - 1] + 'no-repeat center center / 76%';
 		};
 
 
